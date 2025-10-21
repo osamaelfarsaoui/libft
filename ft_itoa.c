b5aa193e57487c12c0b674aa063c28d75ba408f4
Fornax <realfornax@gmail.com>
2025-10-21 21:47:35 +0100

    fixed ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,17 +6,19 @@
 /*   By: fornax <fornax@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 13:02:57 by oelfarsa          #+#    #+#             */
-/*   Updated: 2025/10/21 19:11:54 by fornax           ###   ########.fr       */
+/*   Updated: 2025/10/21 21:38:40 by fornax           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	count(int nb)
+static	int	count(int n)
 {
 	int	count;
+	long	nb;
 
 	count = 0;
+	nb = (long)n;
 	if (nb == 0)
 		count++;
 	if (nb < 0)
@@ -36,23 +38,25 @@ char	*ft_itoa(int n)
 {
 	int		len;
 	char	*alloc;
+	long	nb;
 
-	len = count(n);
+	nb = (long)n;
+	len = count(nb);
 	alloc = malloc(sizeof(char) * (len + 1));
 	if (!alloc)
 		return (NULL);
 	alloc[len--] = 0;
-	if(n == 0)
+	if(nb == 0)
 		alloc[0] = '0';
-	if(n < 0)
+	if(nb < 0)
 	{
-		n = -n;
+		nb = -nb;
 		alloc[0] = '-';
 	}
-	while(n != 0)
+	while(nb != 0)
 	{
-		alloc[len--] = (n % 10) + 48;
-		n /= 10;
+		alloc[len--] = (nb % 10) + 48;
+		nb /= 10;
 	}
 	return (alloc);
 }
